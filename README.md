db674c58bc5e63d39e244a4cfd2f35686552e79a
Salome Gonzalez <alu0100507041@ull.edu.es>
2013-10-01 15:35:43 +0100

    Iniciando practica

---
+++ README.md
@@ -0,0 +1,4 @@
+- Asignatura: Sistemas y Tecnologias Web
+- Practica: Accediendo a Twitter y mostrando los ultimos twitts en una pagina
+
+
---
+++ configure.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'clV3TL9BTkIBTGY2CLe2Q'
+  config.consumer_secret = 'gt4XHBxE9nmmgaOW5GAhCFkOhfBLpgaE6MnoKpy3fQ'
+  config.oauth_token =	'226706276-2mdDkgZLU5XfaHdVGDXPLwku1BOWTkwPAUazzeqB'
+  config.oauth_token_secret = 'wTUQptUshLZclQD1AgalpoFYYZoIRtR4JFfV5r6cc'
+end
